20440c0a5bd1c93a11112cbd30ed5d6f2a8ae70d
Pallavi Priya <75567523+pallavipriyaa@users.noreply.github.com>
2021-05-25 07:24:11 -0700

    Added index page
    This file has errors. Solving......

---
+++ index.php
@@ -0,0 +1,51 @@
+<html>
+	<head>
+		<title> | MUZIKO.COM | </title>
+		<link rel="stylesheet" href="muziko.css">
+	</head>
+	
+	<body>
+		<div class="container">
+		<div class="navbar">
+			<img src="C:\Users\Roshan\Desktop\MUSKAN\muzikowhite.png" class="logo">
+
+				<ul>
+				<li><a href="signup.html">SIGN UP</a></li>
+				<li><a href="login.html">LOGIN</a></li>
+				</ul>
+	</div>
+	<div id="pic">
+	<img src="C:\Users\Roshan\Desktop\MUSKAN\music.jpg" width="1355px" height="500px">
+				<!--######################     IMAGE INSERTION    ########################-->
+	</div>
+				<!--######################     FOOTER SECTION     ########################-->
+	<div id="footer"> 
+	<img src="C:\Users\Roshan\Desktop\MUSKAN\muzikoblack.png" class="logo">
+		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+	<h2>COMPANY   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	    COMMUNITIES  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+		USEFUL LINKS<br>
+<a  href="about.html">About</a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a  href="artists.html">For Artists</a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="support.html">Support</a>&nbsp;&nbsp;&nbsp;
+             <br>
+<a href="jobs.html">Jobs</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="developers.html">Developers</a>
+                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="webplayer.html">Web Player</a>&nbsp;&nbsp;&nbsp;
+             <br>
+<a href="record.html">For the Record</a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="advertising.html">Advertising</a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+<a href="mobileapp.html">Free Mobile App</a>
+                 <br>
+				<a href="investors.html">Investors</a>
+                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+				 
+</div>
+</body>
+</html

af6c90ca02ac63d1f483a8c1b9e36027225085c0
Pallavi Priya <75567523+pallavipriyaa@users.noreply.github.com>
2021-05-25 08:10:38 -0700

    index page
    added index page

--- index.php
+++ index.php
@@ -1,51 +1,67 @@
 <html>
 	<head>
-		<title> | MUZIKO.COM | </title>
-		<link rel="stylesheet" href="muziko.css">
+		<title>MUZIKO | Home</title>
+		<link rel="stylesheet" href="index.css">
 	</head>
 	
 	<body>
 		<div class="container">
 		<div class="navbar">
-			<img src="C:\Users\Roshan\Desktop\MUSKAN\muzikowhite.png" class="logo">
-
+			<img src="images/logo/white1.jpg" class="logo">
 				<ul>
-				<li><a href="signup.html">SIGN UP</a></li>
-				<li><a href="login.html">LOGIN</a></li>
+				    <li><a href="signup.html">SIGN UP</a></li>
+				    <li><a href="login.html">LOGIN</a></li>
 				</ul>
-	</div>
-	<div id="pic">
-	<img src="C:\Users\Roshan\Desktop\MUSKAN\music.jpg" width="1355px" height="500px">
-				<!--######################     IMAGE INSERTION    ########################-->
-	</div>
-				<!--######################     FOOTER SECTION     ########################-->
-	<div id="footer"> 
-	<img src="C:\Users\Roshan\Desktop\MUSKAN\muzikoblack.png" class="logo">
-		&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
-	<h2>COMPANY   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-	    COMMUNITIES  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-		USEFUL LINKS<br>
-<a  href="about.html">About</a>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a  href="artists.html">For Artists</a>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="support.html">Support</a>&nbsp;&nbsp;&nbsp;
-             <br>
-<a href="jobs.html">Jobs</a>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="developers.html">Developers</a>
-                &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="webplayer.html">Web Player</a>&nbsp;&nbsp;&nbsp;
-             <br>
-<a href="record.html">For the Record</a>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="advertising.html">Advertising</a>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-<a href="mobileapp.html">Free Mobile App</a>
-                 <br>
-				<a href="investors.html">Investors</a>
-                 &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+	    </div>
+	    <div id="pic">
+	        <img src="images/bg.jpeg" width="1355px" height="500px">
+	    </div>
+		
+        <!--######################     FOOTER SECTION     ########################-->
+	   
+        <footer>
+            <div class="footer">
+                <div class="footer-logo">
+                    <img src="images/logo/white1.png" alt="logo" class="logo">
+                </div>
+                <br>
+                <div class="footer-links">
+                    <table>
+                        <tr>
+                            <th>COMPANY</th>
+                            <th>COMMUNITIES</th>
+                            <th>USEFUL LINKS</th>                   
+                        </tr>
+                        <tr>
+                            <td><a  href="about.html">About</a></td>    
+                            <td><a  href="artists.html">For Artists</a></td>
+                            <td><a href="support.html">Support</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="jobs.html">Jobs</a></td>
+                            <td><a href="developers.html">Developers</a></td>
+                            <td><a href="webplayer.html">Web Player</a></td>
+                        </tr>
+                        <tr>
+                            <td><a href="record.html">For the Record</a></td>
+                            <td><a href="advertising.html">Advertising</a></td>
+                            <td><a href="mobileapp.html">Free Mobile App</a></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><a href="investors.html">Investors</a></td>
+                            <td></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td><a href="Vendors.html">Vendors</a></td>
+                            <td></td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
+        </footer>
 				 
-</div>
+    </div>
 </body>
 </html

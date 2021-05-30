1650c31b464f09811dc090a41ac443af23bf4d82
Pallavi Priya <75567523+pallavipriyaa@users.noreply.github.com>
2021-05-30 07:41:45 -0700

    updated home page
    home.php

--- home.php
+++ home.php
@@ -11,43 +11,43 @@
  
 
 
-        <div class="container">
-            <!-- ********* FIRST IMAGE ******* -->
-            <div class="content">
-                <a href="#"><figure class="img1"><img src="images/arijit2.jpg" alt="Indian Music" class="image1"></figure>
-                <div class="img-overlay">
-                <div class="img-title">Indian Music</div>
-                    <p class="img-des">Listen to your favourite Indian Music here..</p>
+            <div class="container">
+                <!-- ********* FIRST IMAGE ******* -->
+                <div class="content">
+                    <a href="#"><figure class="img1"><img src="images/arijit2.jpg" alt="Indian Music" class="image1"></figure>
+                    <div class="img-overlay">
+                    <div class="img-title">Indian Music</div>
+                        <p class="img-des">Listen to your favourite Indian Music here..</p>
+                    </div>
+                    </a>
+                    
                 </div>
-                </a>
-                
+                <!-- ********* SECOND IMAGE ******* -->
+                <div class="content">
+                    <a href="#"><figure class="img2"><img src="images/avamax.jpg" alt="International Music" class="image2"></figure>
+                    <div class="img-overlay">
+                        <div class="img-title">International Music</div>
+                        <p class="img-des">Listen to your favourite International Music here..</p>
+                    </div>
+                    </a>
+                </div> 
+
             </div>
-            <!-- ********* SECOND IMAGE ******* -->
-            <div class="content">
-                <a href="#"><figure class="img2"><img src="images/avamax.jpg" alt="International Music" class="image2"></figure>
-                <div class="img-overlay">
-                    <div class="img-title">International Music</div>
-                    <p class="img-des">Listen to your favourite International Music here..</p>
-                </div>
-                </a>
-            </div> 
+            <br><br><br><br>
+            <center>
+                <button><a href="signup.php">Log Out</a></button>
+            </center>
+            <br><br>
 
-        </div>
-        <br><br><br><br>
-        <center>
-            <button><a href="signup.php">Log Out</a></button>
-        </center>
-        <br><br>
-
-        <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
+            <div class="wave waveTop" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-top.png')"></div>
         </div>
         <div class="waveWrapperInner bgMiddle">
-        <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
+            <div class="wave waveMiddle" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-mid.png')"></div>
         </div>
         <div class="waveWrapperInner bgBottom">
-        <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
+            <div class="wave waveBottom" style="background-image: url('http://front-end-noobs.com/jecko/img/wave-bot.png')"></div>
         </div>
-        </div>
+    </div>
 
     
 </body>

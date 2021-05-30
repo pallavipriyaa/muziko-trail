29ea2ff2385e1b84e9d468891b7389ac41373078
Pallavi Priya <75567523+pallavipriyaa@users.noreply.github.com>
2021-05-30 01:38:45 -0700

    Landing Page

--- international.php
+++ international.php
@@ -1,8 +1,28 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>International Music | Muziko</title>
-        <link href="css/international.css" rel="stylesheet" type="text/css"/>
+<head>
+    <title>International Music | Muziko</title>
+    <link href="css/international.css" rel="stylesheet" type="text/css"/>
 
-    </head>
+</head>
+<body>
+    <!-- *******  HEADER  *******-->
+    <div>
+
+    </div>
+    <!-- *******  CAROUSEL  *******-->
+    <div>
+        <div>
+
+        </div>
+    </div>
+    
+    <div class="container">
+        <div class="content">
+
+        </div>
+
+    </div>
+        
+</body>
 </html>
---
+++ landing.php
@@ -0,0 +1,57 @@
+<html>
+<head>
+<title>MUZIKO</title>
+<link rel="stylesheet" href="css/landing.css">
+</head>
+
+<body bgcolor="black">
+    <div class="container">
+        <div class="navbar">
+            <img src="assets/images/logo1.png" class="logo">
+            <ul>
+                <li><a href="signup.php">SIGN UP</a></li>
+                <li><a href="index.php">LOGIN</a></li>
+            </ul>
+        </div>
+        <div id="pic">
+            <img src="assets/images/taylor.jpg"  height="950px" style="opacity:0.5;">
+            <div class="caption TL" ><p> Your favourite songs <br> Anytime<br> Anywhere<br><br>
+            &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<a href="signup.php" class="button">GET MUZIKO FREE</a>
+        </div>
+    </div>
+    <!--######################     FOOTER SECTION     ########################-->
+    <div id="footer" style="height:170px;">
+    <img src="assets/images/logo2.png" class="logo">
+    <center>
+    <table>
+        <tr>
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<br>
+        <th>COMPANY</th>   &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <th>COMMUNITIES</th>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <th>USEFUL LINKS</th>
+        </tr>
+        <tr>
+        <td><a  href="about.html" >About</a></td><td></td>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a  href="artists.html">For Artists</a></td><td></td>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a href="support.html">Support</a></td><td></td></tr>&nbsp;&nbsp;&nbsp;
+                    <br>
+                    <tr>
+        <td><a href="jobs.html">Jobs</a></td><td></td>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a href="developers.html">Developers</a></td><td></td>
+                    ;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a href="webplayer.html">Web Player</a></td><td></td></tr>&nbsp;&nbsp;&nbsp;
+        <tr>             
+        <td><a href="record.html">Record</a></td><td></td>
+                        &nbsp;&nbsp;&nbsp;&nbsp&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a href="advertising.html">Advertising</a></td><td></td>
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        <td><a href="mobileapp.html">Free Mobile App</a><td><td></td></tr>
+                        
+    </table>
+    </center>
+    </div>
+</body>
+</html>

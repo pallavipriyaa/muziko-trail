91024f8e821dedecb25338c5834309856665e758
Pallavi Priya <75567523+pallavipriyaa@users.noreply.github.com>
2022-08-11 12:48:27 +0530

    Update indian.php
    Added all the images on the website.

--- indian.php
+++ indian.php
@@ -11,8 +11,8 @@
 			<img id="img1" src="images\logo\white1.png">
 
 				<ul>
-				<li><a href="home.html">HOME</a></li>
-				<li><a href="logout.html">LOGOUT</a></li>
+				<li><a href="home.php">HOME</a></li>
+				<li><a href="landing.php">LOGOUT</a></li>
 				</ul>
 	</div>
 	<center>
@@ -30,28 +30,28 @@
 			<input type="radio" name="radio-btn" id="radio8">
 				
 				<div class="slide first">
-					<img src="rap1.png">
+					<img src="images\indian\rap1.png">
 				</div>
 				<div class="slide">
-					<img src="indie2.png">
+					<img src="images\indian\indie2.png">
 				</div>
 				<div class="slide">
-					<img src="work3.png">
+					<img src="images\indian\work3.png">
 				</div>
 				<div class="slide">
-					<img src="top4.png">
+					<img src="images\indian\top4.png">
 				</div>
 				<div class="slide">
-					<img src="workout5.png">
+					<img src="images\indian\workout5.png">
 				</div>
 				<div class="slide">
-					<img src="punjabi6.png">
+					<img src="images\indian\punjabi6.png">
 				</div>
 				<div class="slide">
-					<img src="retro7.png">
+					<img src="images\indian\retro7.png">
 				</div>
 				<div class="slide">
-					<img src="top8.png">
+					<img src="images\indian\top8.png">
 				</div>
 			
 			<div class="navigation-auto">
@@ -91,13 +91,13 @@
 
 	<div id="container">
 	<h1>Artists Of The Month</h1>
-	<div id="artist"><a href="#"><img id="img2" src="arijit.jpg"></a>
+	<div id="artist"><a href="#"><img id="img2" src="images\indian\arijitsingh.jpg"></a>
 	<h2>Arijit Singh</h2></div>
-	<div id="artist"><a href="#"><img id="img2" src="neha.jpg"></a>
+	<div id="artist"><a href="#"><img id="img2" src="images\indian\neha.jpg"></a>
 	 <h2>Neha Kakkar</h2></div>
-	<div id="artist"><a href="#"><img id="img2" src="stebin.jpg"></a>
+	<div id="artist"><a href="#"><img id="img2" src="images\indian\stebin.jpeg"></a>
 	<h2>Stebin Ben</h2></div>
-	<div id="artist"><a href="#"><img id="img2" src="tulsi.jpg"></a>
+	<div id="artist"><a href="#"><img id="img2" src="images\indian\tulsi.png"></a>
 	 <h2>Tulsi Kumar</h2></div>
 	</div>
 					<!--#####################		PLAYLIST SECTION	########################-->
@@ -105,106 +105,106 @@
 	<div id="horizontal_slider"><h1>Genres</h1>
 	<div id="slider_content">
 	<div id="content">
-	<a href="#"><img id="img4" src="bollywood.png">
+	<a href="#"><img id="img4" src="images\indian\bollywood.png">
 		<h3> Bollywood Music</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="punjabipop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\punjabipop.png">
 		<h3> Punjabi Pop</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="hiphop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\hiphop.png">
 		<h3> Hip Hop</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="indianblues.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\indianblues.png">
 		<h3> Indian Blues</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="rock.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\rock.png">
 		<h3> Rock</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="indiepop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\indiepop.png">
 		<h3> Indie Pop</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="classical.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\classical.png">
 		<h3> Classical</h3></div></div></div>
 					<!--#####################		PICK YOUR MOOD	########################-->
 	
 	<div id="horizontal_slider"><h1>Pick Your Mood</h1>
 	<div id="slider_content">
 	<div id="content">
-	<a href="#"><img id="img4" src="workout.png">
+	<a href="#"><img id="img4" src="images\indian\workout.png">
 		<h3> Morning Mood</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="romantic.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\romantic.png">
 		<h3> Romance</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="workaholic.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\workaholic.png">
 		<h3> Workaholic</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="wedding.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\wedding.png">
 		<h3> Wedding</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="lofi.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\lofi.png">
 		<h3> Lo-Fi</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="festival.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\festival.png">
 		<h3> Festival</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="retro.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\retro.png">
 		<h3> Retro</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="rap.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\rap.png">
 		<h3> Rap</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="vintage.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\vintage.png">
 		<h3> Vintage</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="devotional.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\devotional.png">
 		<h3> Devotional</h3></div></div></div>
 					<!--#####################		TOP CHARTS	########################-->
 		
 	<div id="horizontal_slider"><h1>Top Charts</h1>
 	<div id="slider_content">
-	<div id="content"><a href="#"><img id="img4" src="1.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\1.png">
 		<h3> TOP 100 <br> Muziko</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="2.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\2.png">
 		<h3> TOP 20 <br> Romantic</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="3.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\3.png">
 		<h3> TOP 50 <br> Weekly</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="4.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\4.png">
 		<h3> TOP 10 <br> Ghazal</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="5.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\5.png">
 		<h3> TOP 20 <br> Dance</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="6.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\6.png">
 		<h3> TOP 10 <br> Hot Hits</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="7.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\7.png">
 		<h3> TOP 20 <br> Punjabi</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="8.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\8.png">
 		<h3> TOP 10 <br> Retro</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="9.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\9.png">
 		<h3> TOP 10 <br> Trending</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="10.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\10.png">
 		<h3> TOP 20 <br> Chartbusters</h3></div></div></div>
 					<!--#####################		WEEKLY JUKEBOX	########################-->
 		
 	<div id="horizontal_slider"><h1>Weekly Jukebox</h1>
 	<div id="slider_content">
 	<div id="content">
-	<a href="#"><img id="img4" src="monday.png">
+	<a href="#"><img id="img4" src="images\indian\monday.png">
 		<h3> Monday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="tuesday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\tuesday.png">
 		<h3> Tuesday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="wednesday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\wednesday.png">
 		<h3> Wednesday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="thursday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\thursday.png">
 		<h3> Thursday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="friday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\friday.png">
 		<h3> Friday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="saturday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\saturday.png">
 		<h3> Saturday</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="sunday.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\sunday.png">
 		<h3> Sunday</h3></div></div></div>
 					<!--#####################		PODCASTS	########################-->
 		
 	<div id="horizontal_slider"><h1>PODCASTS</h1>
 	<div id="slider_content">
 	<div id="content">
-	<a href="#"><img id="img4" src="bollywood.png">
+	<a href="#"><img id="img4" src="images\indian\bollywood.png">
 		<h3> BOLLYWOOD MUSIC</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="punjabipop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\punjabipop.png">
 		<h3> PUNJABI POP</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="hiphop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\hiphop.png">
 		<h3> HIP HOP</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="indianblues.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\indianblues.png">
 		<h3> INDIAN BLUES</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="rock.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\rock.png">
 		<h3> ROCK</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="indiepop.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\indiepop.png">
 		<h3> INDIE POP</h3></div>
-	<div id="content"><a href="#"><img id="img4" src="classical.png">
+	<div id="content"><a href="#"><img id="img4" src="images\indian\classical.png">
 		<h3> CLASSICAL</h3></div></div></div>
 <!--########################## FOOTER SECTION ###########################-->
     <footer>
